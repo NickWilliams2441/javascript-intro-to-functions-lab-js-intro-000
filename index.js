596b572181dd056fb4b4792c0c3f3b36c73b5e5b
Nicholas Williams <nickdwilliams96@gmail.com>
2020-07-28 01:33:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(shout){
 function logWhisper(whisper){
   console.log(whisper.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED"
+  }
+  if (string === "I love you, too.") {
+    return "I love you, Grandma."
+  }
+  
+}
